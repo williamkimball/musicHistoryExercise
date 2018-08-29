5ca4a39618a69d85e4d6f9f9546b738693225573
williamkimball <36681575+williamkimball@users.noreply.github.com>
2018-08-29 11:36:04 -0500

    worked on first few problems

---
+++ musichistory.sql
@@ -0,0 +1,20 @@
+SELECT * 
+FROM Genre;
+
+SELECT * 
+FROM Artist;
+
+INSERT INTO Artist 
+(ArtistName, YearEstablished)
+VALUES 
+("Jisie",1989);
+
+DELETE FROM Artist
+WHERE ArtistName='Jisie';
+
+INSERT INTO Artist 
+(ArtistName, YearEstablished)
+VALUES 
+("Jisie",1989);
+
+SELECT * FROM Artist;
